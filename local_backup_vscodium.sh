e529478253edfeca90068354168f7ce8953a9e48
elppans <elppansmk@gmail.com>
2025-03-01 00:39:59 -0300

    Update and rename backup_vscodium.sh to local_backup_vscodium.sh

--- local_backup_vscodium.sh
+++ local_backup_vscodium.sh
@@ -2,7 +2,7 @@
 
 # Diretórios e arquivos importantes
 CONFIG_DIR="$HOME/.config/VSCodium/User"
-BACKUP_DIR="$HOME/Backup_VSCodium"
+BACKUP_DIR="$HOME/.config/VSCodium/Backup_VSCodium"
 EXTENSIONS_FILE="extensions_list.txt"
 BACKUP_FILE="vscodium_backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 
@@ -12,8 +12,13 @@ mkdir -p "$BACKUP_DIR"
 # Salvar a lista de extensões instaladas
 codium --list-extensions > "$BACKUP_DIR/$EXTENSIONS_FILE"
 
-# Criar o pacote de backup incluindo configurações e lista de extensões
-tar -czvf "$BACKUP_DIR/$BACKUP_FILE" -C "$CONFIG_DIR" . -C "$BACKUP_DIR" "$EXTENSIONS_FILE"
+# Criar o pacote de backup incluindo configurações e lista de extensões, excluindo workspaceStorage e History
+tar --exclude="globalStorage" \
+--exclude="History" \
+--exclude="workspaceStorage" \
+-czvf "$BACKUP_DIR/$BACKUP_FILE" \
+-C "$CONFIG_DIR" . \
+-C "$BACKUP_DIR" "$EXTENSIONS_FILE"
 
 # Remover o arquivo temporário da lista de extensões
 rm "$BACKUP_DIR/$EXTENSIONS_FILE"
